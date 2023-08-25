4365acfb1f58c0e546af4706106e89362a3311f8
Vinícius.People.Py <109100850+Vinicius-R-PeoplePy@users.noreply.github.com>
2023-08-25 17:49:37 -0300

    Update exercíciosguanabara.py

--- exercíciosguanabara.py
+++ exercíciosguanabara.py
@@ -413,7 +413,7 @@ print('-=-' * 20)
 print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
 print('=' * 20)
 jogador = int(input('Em que número eu pensei? '))
-PRINT('PROCESSANDO...')
+print('PROCESSANDO...')
 sleep(10)
 if jogador == computador:
 	print('PARABÉNS! Você conseguiu me vencer!')
